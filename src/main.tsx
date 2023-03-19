73c0cf407a205c4275c9acad07c271ee259c9266
Alex <alexmihailov91@gmail.com>
2023-03-19 20:02:46 +0500

    refactor: main

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import About from "./Pages/About/About";
 import Page404 from "./Pages/Page404/Page404";
-// import Main from "./components/Main/Main";
 import {
   BrowserRouter,
   createBrowserRouter,
